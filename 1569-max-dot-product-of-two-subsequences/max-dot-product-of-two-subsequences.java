121e72c463745a6e89e531a9416f1477845faa98
Aditya-2026 <adityadahake2004@gmail.com>
2026-01-08 19:22:07 +0530

    Time: 124 ms (5.19%) | Memory: 46.9 MB (22.22%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
@@ -19,16 +19,32 @@ class Solution {
             pick_i = Math.max(pick_i,(nums1[i]*nums2[k])+solve(i+1,k+1,nums1,nums2,n,m,dp));
         }
         int notPick_i = solve(i+1,j,nums1,nums2,n,m,dp);
-
         return dp[i][j] = Math.max(pick_i,notPick_i);
     }
     public int maxDotProduct(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
-        int[][] dp = new int[n][m];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[n+1][m+1];
+        dp[n][0] = Integer.MIN_VALUE;
+
+        for(int j=1;j<=m;j++){
+            dp[n][j] = 0;
         }
-        return solve(0,0,nums1,nums2,n,m,dp);
+        for(int i=0;i<=n;i++){
+            dp[i][m] = 0;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int pick_i = Integer.MIN_VALUE;
+                for(int k =j;k<m;k++){
+                    pick_i = Math.max(pick_i,(nums1[i]*nums2[k])+dp[i+1][k+1]);
+                }
+                int notPick_i = dp[i+1][j];
+                dp[i][j] = Math.max(pick_i,notPick_i);
+            }
+        }
+
+        return dp[0][0];
     }
 }
