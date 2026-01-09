23e0e6d2cdd198b90c8fffdb4216438e5e846f2b
Aditya-2026 <adityadahake2004@gmail.com>
2026-01-09 20:09:03 +0530

    Time: 1 ms (38.98%) | Memory: 45 MB (25.86%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,21 +1,4 @@
 class Solution {
-    public int helper(int step,int[] cost,int dp[]){
-        if(step==0 || step==1){
-            return cost[step];
-        }
-        if(dp[step] != -1){
-            return dp[step];
-        }
-
-        int way1 = helper(step-1,cost,dp);
-        int way2 = Integer.MAX_VALUE;
-        if(step>1){
-            way2 = helper(step-2,cost,dp);
-        }
-
-        return dp[step] = cost[step] + Math.min(way1,way2);
-    }
-
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int dp[] = new int[n];
