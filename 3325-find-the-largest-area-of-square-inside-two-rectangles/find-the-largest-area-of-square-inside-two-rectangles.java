2d7a9fee1314bab65b829bbef50ff6895f3a04c7
Aditya-2026 <adityadahake2004@gmail.com>
2026-01-17 13:55:50 +0530

    Time: 77 ms (56.25%) | Memory: 47.8 MB (66.67%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
@@ -1,30 +1,34 @@
 class Solution {
     public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
-        int maxSide=0;
         int n = bottomLeft.length;
-        long area = 0;
+        long maxArea = 0;
+
         for(int i=0;i<n;i++){
+            int x1 = bottomLeft[i][0];
+            int y1 = bottomLeft[i][1];
+            int x2 = topRight[i][0];
+            int y2 = topRight[i][1];
+
             for(int j=i+1;j<n;j++){
-                int x1 = bottomLeft[i][0];
-                int y1 = bottomLeft[i][1];
-                int x2 = topRight[i][0];
-                int y2 = topRight[i][1];
                 int x3 = bottomLeft[j][0];
-                int y3 = bottomLeft[j][1];
                 int x4 = topRight[j][0];
-                int y4 = topRight[j][1];
 
                 int xOverlap = Math.min(x2,x4) - Math.max(x1,x3);
-                int yOverlap = Math.min(y2,y4) - Math.max(y1,y3);
-                
+                if(xOverlap<=0) continue;
 
-                if(xOverlap>0 && yOverlap>0){
-                    int side = Math.min(xOverlap,yOverlap);
-                    area = Math.max(area,side*1L*side);
-                }
+                int y3 = bottomLeft[j][1];
+                int y4 = topRight[j][1];
+
+                int yOverlap = Math.min(y2,y4) - Math.max(y1,y3);
+                if(yOverlap<=0) continue;
+                
+                int side = Math.min(xOverlap,yOverlap);
+                long area = (long)side * side;
+
+                maxArea = Math.max(maxArea,area);
             }
         }
 
-        return area;
+        return maxArea;
     }
 }
